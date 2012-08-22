e9027da1ad8bd6cceceba877f78e668ed0af9d81
Fogest <fogestjv@gmail.com>
2012-08-22 15:31:48 -0400

    Adding Database class

---
+++ MCTrade/src/me/fogest/mctrade/Database.java
@@ -0,0 +1,9 @@
+package me.fogest.mctrade;
+
+public class Database {
+	private String dbHost = "localhost";
+	private String dbUsername = "root";
+	private String dbPassword = "";
+	
+	private String dbTable = "mctrade";
+}
